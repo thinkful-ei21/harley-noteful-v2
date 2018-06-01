b0e1a2db5b5a203b754df173be5ee5596ecde04b
harleyjj <harleyyesm@gmail.com>
2018-06-01 09:03:38 -0700

    implemented tags get, get by id, and put endpoints

--- routes/tags.js
+++ routes/tags.js
@@ -4,6 +4,63 @@ const express = require('express');
 const knex = require('../knex');
 const router = express.Router();
 
+router.get('/', (req, res, next) => {
+  knex.select('id', 'name')
+    .from('tags')
+    .then(results => {
+      res.json(results);
+    })
+    .catch(err => next(err));
+});
+  
+router.get('/:id', (req, res, next) => {
+  const id = req.params.id;
+    
+  knex
+    .select('tags.id', 'name')
+    .from('tags')
+    .where('tags.id', id)
+    .then(items => {
+      if (items[0]) {
+        res.json(items[0]);
+      } else {
+        next();
+      }
+    })
+    .catch(err => {
+      next(err);
+    });
+});
+  
+router.put('/:id', (req, res, next) => {
+  const id = req.params.id;
+    
+  /***** Never trust users - validate input *****/
+  const updateObj = {};
+  if ('name' in req.body) {
+    updateObj['name'] = req.body.name;
+  } else {
+    const err = new Error('Missing `name` in request body');
+    err.status = 400;
+    return next(err); 
+  }
+    
+  knex('tags')
+    .where('tags.id', id)
+    .update(updateObj)
+    .returning(['tags.id', 'name'])
+    .then(items => {
+      if (items[0]) {
+        res.json(items[0]);
+      } else {
+        next();
+      }
+    })
+    .catch(err => {
+      next(err);
+    });
+});
+
 router.post('/', (req, res, next) => {
   const { name } = req.body;
     
